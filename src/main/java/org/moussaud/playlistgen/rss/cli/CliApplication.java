ee049e14d15c5c46004fafd5bd9594c872e5dcc3
Benoit Moussaud <benoit@moussaud.org>
2024-04-24 10:22:56 +0200

    initial commit

---
+++ src/main/java/org/moussaud/playlistgen/rss/cli/CliApplication.java
@@ -0,0 +1,13 @@
+package org.moussaud.playlistgen.rss.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/moussaud/playlistgen/rss/cli/CliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moussaud.playlistgen.rss.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
